5a617f7e8102cc2808795c2a53c1318e21924e0a
Bradley Ray <bradleyr320@gmail.com>
2023-06-18 05:54:44 -0500

    clean up sd lib

--- lib/sd/sd.c
+++ lib/sd/sd.c
@@ -6,76 +6,84 @@ static uint8_t sd_cmd8[6] = {0x48, 0x00, 0x00, 0x01, 0xaa, 0x87};
 static uint8_t sd_cmd58[6] = {0x7a, 0x00, 0x00, 0x00, 0x00, 0xff};
 static uint8_t sd_cmd55[6] = {0x77, 0x00, 0x00, 0x00, 0x00, 0xff};
 static uint8_t sd_acmd41[6] = {0x69, 0x40, 0x00, 0x00, 0x00, 0xff};
-// static uint8_t sd_cmd9[6] = {0x49, 0x00, 0x00, 0x00, 0x00, 0xff};
 static uint8_t sd_cmd17[6] = {0x40 | 17, 0x00, 0x00, 0x00, 0x00, 0xff};
 static uint8_t sd_cmd24[6] = {0x40 | 24, 0x00, 0x00, 0x00, 0x00, 0xff};
-// static uint8_t sd_cmd13[6] = {0x40 | 13, 0x00, 0x00, 0x00, 0x00, 0xff};
-// static uint8_t sd_acmd22[6] = {0x40 | 22, 0x00, 0x00, 0x00, 0x00, 0xff};
 
 uint8_t sd_send_cmd(uint8_t* cmd) {
-	for(uint32_t i = 0; spi_read_write_byte(0xff) != 0xff && i < 512; ++i);
-
-	spi_read_write_buff(cmd, NULL, 6);
-
+	// wait until not busy
 	uint8_t response;
-	while((response = spi_read_write_byte(0xff)) & 0x80);
+	do {
+		sd_read(&response, 1);
+	} while(response != 0xff);
+
+	// send cmd
+	sd_write(cmd, 6);
+
+	// wait for R1 response
+	do {
+		sd_read(&response, 1);
+	} while(response & 0x80);
 
 	return response;
 }
 
+// TODO: should this be part of sd hal layer?
+// make sure CS is high when called
 void sd_preinit(void) {
+	uint8_t cmd = 0xff;
 	for (uint32_t i = 0; i < 10; ++i)
-		spi_read_write_byte(0xff);
+		sd_write(&cmd, 1);
 }
 
-void sd_init(void) {
-	// send CMD0
+// SD Card initialization sequence
+void sd_init(uint8_t* rx_buff) {
 	while(sd_send_cmd(sd_cmd0) != 0x1);
 
-	// send CMD8
 	while(sd_send_cmd(sd_cmd8) != 0x1);
-	for (uint32_t i = 0; i < 4; ++i)
-		spi_read_write_byte(0xff);
+	sd_read(rx_buff, 4);
 
-	// send CMD58 get device data
 	while(sd_send_cmd(sd_cmd58) != 0x1);
-	for (uint32_t i = 0; i < 4; ++i)
-		spi_read_write_byte(0xff);
+	sd_read(rx_buff, 4);
 
-	// send CMD55, ACMD41 until not idle
 	do {
 		sd_send_cmd(sd_cmd55);
 	} while (sd_send_cmd(sd_acmd41) != 0);
 
-	// send CMD58
 	while(sd_send_cmd(sd_cmd58) != 0x0);
-	for (uint32_t i = 0; i < 4; ++i)
-		spi_read_write_byte(0xff);
+	sd_read(rx_buff, 4);
 }
 
-void sd_read_block(uint32_t addr, uint8_t* buff) {
+// Read a block from SD Card Memory
+void sd_read_block(uint32_t addr, uint8_t* rx_buff) {
 	sd_cmd17[1] = (uint8_t)((addr) >> 24);
 	sd_cmd17[2] = (uint8_t)((addr) >> 16);
 	sd_cmd17[3] = (uint8_t)((addr) >> 8);
 	sd_cmd17[4] = (uint8_t)((addr));
 
 	while(sd_send_cmd(sd_cmd17) != 0);
-	while(spi_read_write_byte(0xff) != 0xfe);
+	uint8_t response;
+	do {
+		sd_read(&response, 1);
+	} while(response != 0xfe);
 
-	for (uint32_t i = 0; i < 512; ++i)
-		buff[i] = 0;
-	for (uint32_t i = 0; i < 512; ++i) 
-		buff[i] = spi_read_write_byte(0xff);
+	sd_read(rx_buff, 512);
 }
 
-void sd_write_block(uint32_t addr, uint8_t* buff) {
+// Write a block to SD Card Memory
+void sd_write_block(uint32_t addr, uint8_t* tx_buff) {
 	sd_cmd24[1] = (uint8_t)((addr) >> 24);
 	sd_cmd24[2] = (uint8_t)((addr) >> 16);
 	sd_cmd24[3] = (uint8_t)((addr) >> 8);
 	sd_cmd24[4] = (uint8_t)((addr));
 
 	while(sd_send_cmd(sd_cmd24) != 0);
-	spi_read_write_byte(0xfe);
-	spi_read_write_buff(buff, NULL, 512);
-	while(spi_read_write_byte(0xff) & 0x10);
+
+	uint8_t byte = 0xfe;
+	sd_write(&byte, 1);
+	sd_write(tx_buff, 512);
+
+	uint8_t response;
+	do {
+		sd_read(&response, 1);
+	} while(response & 0x10);
 }
--- lib/sd/sd.h
+++ lib/sd/sd.h
@@ -1,100 +1,31 @@
 #ifndef __SD_h__
 #define __SD_h__
 
-#include <inttypes.h>
-#include <stdio.h>
+#include <stdint.h>
 
 #define SD_CMD0_CRC 0x95
 #define SD_CMD8_CRC 0x87
 
-typedef struct {
-    uint8_t cmd;
-    uint8_t arg1;
-    uint8_t arg2;
-    uint8_t arg3;
-    uint8_t arg4;
-    uint8_t crc;
-    uint8_t resp_type;
-} sd_cmd_struct_t;
-
 typedef enum {
     SD_CMD0,
-    SD_CMD1,
-    SD_CMD2,
-    SD_CMD3,
-    SD_CMD4,
-    SD_CMD5,
-    SD_CMD6,
-    SD_CMD7,
-    SD_CMD8,
+    SD_CMD8 = 8,
     SD_CMD9,
-    SD_CMD10,
-    SD_CMD11,
-    SD_CMD12,
-    SD_CMD13,
-
-    SD_CMD15 = 15,
-    SD_CMD16,
-    SD_CMD17,
-    SD_CMD18,
-
-    SD_CMD20 = 20,
-
+    SD_CMD17 = 17,
     SD_CMD24 = 24,
-    SD_CMD25,
-    SD_CMD26,
-    SD_CMD27,
-    SD_CMD28,
-    SD_CMD29,
-    SD_CMD30,
-    
-    SD_CMD32 = 32,
-    SD_CMD33,
-    
-    SD_CMD38 = 38,
-    SD_CMD39,
-    SD_CMD40,
-
-    
-    SD_CMD42 = 42,
-
     SD_CMD55 = 55,
-    SD_CMD56,
-    
     SD_CMD58 = 58,
-    SD_CMD59,
-
     SD_ACMD41 = 41,
 } sd_cmd_t;
 
-typedef struct {
-    uint8_t version: 2;
-    uint8_t reserved_1: 6;
-    uint8_t taac;
-    uint8_t nsac;
-    uint8_t tran_speed;
-    uint32_t ccc: 12;
-    uint8_t read_bl_len: 4;
-    uint8_t read_bl_partial: 1;
-    uint8_t write_blk_misalign: 1;
-    uint8_t read_blk_misalign: 1;
-    uint8_t dsr_imp: 1;
-    uint8_t reserved_2: 6;
-    uint32_t c_size: 22;
-    uint32_t one;
-    uint32_t two;
-    uint32_t three;
-    uint32_t four;
-    uint32_t five;
-    uint32_t six;
-} csd_response_t;
+void sd_write(uint8_t* tx_buff, uint32_t size);
+void sd_read(uint8_t* rx_buff, uint32_t size);
 
-
-uint8_t sd_send_cmd(uint8_t* cmd);
 void sd_preinit(void);
-void sd_init(void);
+void sd_init(uint8_t* rx_buff);
+uint8_t sd_send_cmd(uint8_t* cmd);
 void sd_read_block(uint32_t addr, uint8_t* buff);
 void sd_write_block(uint32_t addr, uint8_t* buff);
+
 
 
 #endif
